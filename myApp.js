856e669f3fc458bc13a0ab23e3e65cf826aec149
X-fherd <x.fherdinand@gmail.com>
2023-07-26 12:10:59 -0700

    Update

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-app.use(absolutePath = __dirname + "/public", express.static());
+app.use(express.static(absolutePath = _dirname + "/public"));
 
 //console.log("Hello World")
 
