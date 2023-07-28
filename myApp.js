f07ace80e1826fd133defd457cadbfbf5d1e0c6d
X-fherd <x.fherdinand@gmail.com>
2023-07-27 18:31:50 -0700

    Update

--- myApp.js
+++ myApp.js
@@ -11,7 +11,9 @@ app.get("/", function(req, res){
 })
 
 app.get("/json", function(req, res){
-    process.env.MESSAGE_STYLE;
+    if (process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message": "HELLO JSON"})
+    }
     res.json({"message": "Hello json"});
 })
 
